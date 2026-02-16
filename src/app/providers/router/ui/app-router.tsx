5456d679770a4283130e8bf48d1cff0c9ad1a707
dorincreciun <creciundorin25@gmail.com>
2026-02-16 10:46:38 +0200

    fix(router): fix build routes logic

--- src/app/providers/router/ui/app-router.tsx
+++ src/app/providers/router/ui/app-router.tsx
@@ -1,9 +1,7 @@
 import {buildRoutes} from "../lib/build-routes";
 import {ROUTE_CONFIG} from "../config/route-config";
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {useRoutes} from "react-router";
 
 export const AppRouter = () => {
-    const router = createBrowserRouter(buildRoutes(ROUTE_CONFIG));
-
-    return <RouterProvider router={router} />;
+    return useRoutes(buildRoutes(ROUTE_CONFIG));
 };
