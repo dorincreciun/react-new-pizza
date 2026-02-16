ddb740437f4bf4810f34c62fc81dbced18686759
dorincreciun <creciundorin25@gmail.com>
2026-02-16 08:38:35 +0200

    chore: added react-router library in to project

--- src/app/entrypoint/App.tsx
+++ src/app/entrypoint/App.tsx
@@ -2,18 +2,21 @@ import {RootLayout} from "@shared/layouts";
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {QueryClient, QueryClientProvider,} from '@tanstack/react-query'
 import {Header} from "@widgets/header";
+import {BrowserRouter} from "react-router";
 
 const queryClient = new QueryClient()
 
 export const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
-            <RootLayout>
-                <Header />
+            <BrowserRouter>
+                <RootLayout>
+                    <Header />
 
-                {/* Tanstack Devtools */}
-                <ReactQueryDevtools initialIsOpen={false} />
-            </RootLayout>
+                    {/* Tanstack Devtools */}
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </RootLayout>
+            </BrowserRouter>
         </QueryClientProvider>
     )
 }
