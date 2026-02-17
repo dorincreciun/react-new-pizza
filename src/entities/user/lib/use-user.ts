329affbcaa83b9a2ad108c246b4a77fea28de7a2
dorincreciun <creciundorin25@gmail.com>
2026-02-17 15:18:52 +0200

    feat(feat/auth): fix auth sistem

--- src/entities/user/lib/use-user.ts
+++ src/entities/user/lib/use-user.ts
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query"
 
-import { getUser } from "@entities/user"
+import { getUser, useAuthStore } from "@entities/user"
 
 import { QueryKeys } from "@shared/const"
 
 import type { UserEntity } from "../model/types"
 
 export const useUser = (): UserEntity | null => {
+    const token = useAuthStore((s) => s.token)
+
     const { data } = useQuery<UserEntity | null>({
         queryKey: QueryKeys.authUser,
         queryFn: async () => await getUser(),
-        enabled: true,
+        enabled: !!token,
         staleTime: Infinity,
         refetchOnWindowFocus: false,
     })
